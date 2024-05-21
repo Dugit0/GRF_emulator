495b37c0b8070b18b4a86322f13ffbbb45422bcc
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-05-22 00:56:31 +0300

    Add GLOABAL_DEBUG_LOG in core

--- grfemulator/core/__init__.py
+++ grfemulator/core/__init__.py
@@ -5,8 +5,40 @@ import os
 import sys
 from tqdm import tqdm
 import logging
+import traceback
 
 logging.basicConfig(level=logging.WARNING, filename='log.log', filemode='w')
+
+GLOBAL_DEBUG_LOG = ""
+
+# -------------- Custom exception --------------
+class CodeFormatError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+
+class ArgsError(Exception):
+    """
+    Exception raised when expected x arguments and received y (x != y).
+    """
+    def __init__(self, expected, received):
+        self.expected = expected
+        self.received = received
+    def __str__(self):
+        return f"Number of arguments mismatch. Expected - {self.expected}. Received - {self.received}."
+
+
+class DefError(Exception):
+    """
+    Exception raised when function is not defined.
+    """
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"Name '{self.name}' is not defined."
+
 
 # -------------- Parcer functions --------------
 def get_parser(gramm_name):
@@ -22,43 +54,58 @@ def my_parce(code, gramm_name):
     """
     Return AST by code and name of the grammar.
     """
+    code = code.strip()
+    if code == "":
+        raise CodeFormatError("Trying to parce empty code.")
     parcer = get_parser(gramm_name)
     return parcer.parse(code)
 
-
-# -------------- Custom exception --------------
-class ArgsError(Exception):
-    """
-    Exception raised when expected x arguments and received y (x != y).
-    """
-    def __init__(self, expected, received):
-        self.expected = expected
-        self.received = received
-    def __str__(self):
-        return f"number of arguments mismatch. Expected - {self.expected}. Received - {self.received}"
-
-
-class DefError(Exception):
-    """
-    Exception raised when function is not defined.
-    """
-    def __init__(self, name):
-        self.name = name
-    def __str__(self):
-        return f"name '{self.name}' is not defined"
 
 # -------------- Functions --------------
 class Func:
     def __init__(self, n=1, name='Unnamed'):
         self.n = n
         self.name = name
+        self.full_name = ""
         self.call_func = lambda : None
+        self._debug = False
+        self._show_call = False
+        self._show_stack = False
+        self.debug_log = ""
+
+    @property
+    def show_call(self):
+        return self._show_call
+
+    @show_call.setter
+    def show_call(self, flag):
+        self._show_call = flag
+        self._debug = self._show_call or self._show_stack
+
+    @property
+    def show_stack(self):
+        return self._show_stack
+
+    @show_stack.setter
+    def show_stack(self, flag):
+        self._show_stack = flag
+        self._debug = self._show_stack or self._show_stack
+
     def __str__(self):
         return self.name
+
     def __call__(self, *args):
+        global GLOBAL_DEBUG_LOG
         if len(args) != self.n:
             raise ArgsError(self.n, len(args))
-        return self.call_func(*args)
+        if self.show_call:
+            GLOBAL_DEBUG_LOG += f"{self.name}({', '.join(list(map(str, args)))})\n"
+            print(self.__repr__())
+            print(*traceback.format_stack(), sep='----------------------\n')
+            return self.call_func(*args)
+        else:
+        #     return self.optimazed_call(*args)
+            return self.call_func(*args)
 
 
 def func_o():
@@ -111,16 +158,22 @@ def recursion(base, func):
     if base.n + 2 != func.n:
         raise ArgsError(base.n + 2, func.n)
     def new_func(*args):
-        base_args = args[:-1] 
-        result = base(*base_args)
-        for i in range(1, args[-1] + 1):
-            # print([*base_args, i, result])
-            result = func(*[*base_args, i - 1, result])
-            # print(result)
-        return result
+        if args[-1] == 0:
+            return base(*args[:-1])
+        new_args = list(args[:])
+        new_args[-1] = new_args[-1] - 1
+        new_args.append(new_func(*new_args))
+        return func(*new_args)
+    # def new_func(*args):
+    #     base_args = args[:-1] 
+    #     result = base(*base_args)
+    #     for i in range(1, args[-1] + 1):
+    #         result = func(*[*base_args, i - 1, result])
+    #     return result
     res = Func(base.n + 1)
     res.call_func = new_func
     return res
+
 
 def minimisation(func, ind):
     # Test it!
@@ -229,69 +282,4 @@ def parse_call(call, func_dict):
         ans.append((func, args))
         # print('----')
     return tuple(ans)
-
-
-# ------------- Тестироване грамматики -------------
-# path_to_tests = 'examples'
-# for test_filename in sorted(os.listdir(path_to_tests))[1:4]:
-#     with open(os.path.join(path_to_tests, test_filename)) as test_file:
-#         test = test_file.read()
-#     print(f"---------------- TEST {test_filename} ----------------")
-#     test = test.split('!!!')
-#     if len(test) != 2:
-#         print("'!!!' is reserved", file=sys.stderr)
-#         sys.exit(1)
-#     definition, call = test[0], test[1]
-#     print(definition)
-#     print(f"----------------------{len(test_filename) * '-'}-----------------")
-#     print(my_parce(definition, "def_gram.lark").pretty())
-#     # print(my_parce(call, "call_gram.lark"))
-#     print(f"----------------------{len(test_filename) * '-'}-----------------")
-
-
-
-
-if __name__ == "__main__":
-    test = """
-    Sum = I^1_1 <- s { I^3_3 }
-    Mul = o <- Sum { I^3_1 I^3_3 }
-    ElDiff = 0^0 <- I^2_1
-    Diff = I^1_1 <- ElDiff { I^3_3 }
-    AbsDiff = Sum { 
-                  Diff {
-                          I^2_1
-                          I^2_2
-                       }
-                  Diff {
-                          I^2_2
-                          I^2_1
-                       }
-                  }
-    !!!
-Mul(102300, 100)
-    """.strip()
-
-    test = test.split('!!!')
-    if len(test) != 2:
-        print("'!!!' is reserved", file=sys.stderr)
-        sys.exit(1)
-
-    definition, call = test[0], test[1]
-    print(definition)
-    parse_def(definition)
-    print('================================')
-    
-    print(call)
-    called_func = parse_call(call)
-    print('============ RESULT ============')
-    import time
-    for func, args in called_func:
-        start = time.time()
-        print(func(*args))
-        print(time.time() - start)
-
-
-
-
-
 
